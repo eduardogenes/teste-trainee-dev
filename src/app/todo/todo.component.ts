a002ca2876ef916f41c2ce5f3c814eb47052d342
Eduardo Genes <eduardogenes95@gmail.com>
2025-06-07 15:50:46 -0300

    docs: adiciona comentários explicativos nos métodos do TodoComponent

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -13,16 +13,19 @@ export class TodoComponent implements OnInit {
 
   constructor(private todoService: TodoService) { }
 
+  // carrega as tarefas do localStorage
   ngOnInit(): void {
     this.loadTodos();
   }
 
+  // carrega as tarefas do localStorage
   loadTodos() {
     this.todoService.getTodos().subscribe(todos => {
       this.todos = todos;
     });
   }
 
+  // adiciona uma nova tarefa
   addTodo(newTodoTitle: string) {
     const newTodo: Todo = {
       id: this.todos.length + 1,
@@ -33,14 +36,17 @@ export class TodoComponent implements OnInit {
     this.todoService.addTodo(newTodo);
   }
 
+  // atualiza uma tarefa
   updateTodo(updatedTodo: Todo) {
     this.todoService.updateTodo(updatedTodo);
   }
 
+  // remove uma tarefa
   deleteTodo(todoId: number) {
     this.todoService.deleteTodo(todoId);
   }
 
+  // remove todas as tarefas
   clearAll() {
     if (this.todos.length > 0 && confirm('Are you sure you want to clear all tasks?')) {
       this.todoService.clearAll();
@@ -48,21 +54,25 @@ export class TodoComponent implements OnInit {
     }
   }
 
+  // remove as tarefas concluídas
   clearCompletedTasks() {
     this.todoService.clearCompletedTasks();
     this.loadTodos();
   }
 
+  // alterna a visibilidade das tarefas concluídas
   toggleCompletedTasks() {
     this.showCompletedTasks = !this.showCompletedTasks;
     this.loadTodos();
     this.todos = this.filteredTodos();
   }
 
+  // filtra as tarefas concluídas
   filteredTodos() {
     return this.showCompletedTasks ? this.todos : this.todos.filter(todo => !todo.completed);
   }
 
+  // retorna o texto do botão 'Limpar Tudo'
   get labelClearAll(){
     return 'Limpar Tudo'
   }

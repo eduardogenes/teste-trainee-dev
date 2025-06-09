17ef2e5204c5d4ea4619b3b81580ad782f9aee6e
Eduardo Genes <eduardogenes95@gmail.com>
2025-06-09 15:43:03 -0300

    refactor: move HeaderComponent para LayoutModule e organiza imports no app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './layout/header/header.component';
-
 import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+import { LayoutModule } from './layout/layout.module';
 
 @NgModule({
   declarations: [
-    HeaderComponent,
     AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SweetAlert2Module.forRoot(),
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
